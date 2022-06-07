71e3f633514e6b1069056acf1ec3a11b0233a556
Parker <s204814@student.dtu.dk>
2022-06-07 14:05:33 +0200

    Fixed producer/java (virker ikke)

--- src/main/java/Business/Producer.java
+++ src/main/java/Business/Producer.java
@@ -1,4 +1,19 @@
 package Business;
 
-public class Producer {
+import java.util.LinkedList;
+
+public class Producer implements Runnable {
+    //public LinkedList<Integer> queue;
+
+    @Override
+    public void run() {
+        LinkedList<Integer> queue = new LinkedList<>();
+        while (true) {
+            synchronized (queue) {
+
+            }
+
+        }
+
+    }
 }
