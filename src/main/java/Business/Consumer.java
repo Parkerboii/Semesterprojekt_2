660f10c55b4e3b69fa977d937d9e26b25f86711e
Parker <s204814@student.dtu.dk>
2022-06-07 13:55:28 +0200

    Revert "Saving Parker"
    This reverts commit ddcda81c08e0d50144ced0d1decf386841861099.

--- src/main/java/Business/Consumer.java
+++ src/main/java/Business/Consumer.java
@@ -1,35 +1,4 @@
 package Business;
 
-import java.sql.Timestamp;
-import java.util.Queue;
-
-public class Consumer implements Runnable {
-    private Queue<Integer> queue;
-
-    @Override
-    public void run() {
-        while (true) {
-            synchronized (queue) {
-                while(queue.isEmpty()) {
-                    try {
-                        queue.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    int EKG = queue.remove();
-
-                    notify();
-
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-                }
-
-            }
-        }
-    }
-
+public class Consumer {
+}
